e1f26ca2896aff29cc22df248904665118a0e536
leejinhyuk0212 <myjinhyuk@tukorea.ac.kr>
2025-09-19 22:39:12 +0900

    2DGP-Drill03 (삼각운동 추가)
    삼각운동 추가

--- character_moves.py
+++ character_moves.py
@@ -9,16 +9,18 @@ ch = load_image('character.png')
 x = 400
 y = 90
 
-state = 0  # 사각형 방향 상태 0~3
-mode = 0   # 0: 사각형, 1: 원
+state = 0  # 사각형 방향 상태 0~3, 삼각형 방향 상태 0~2
+mode = 0   # 0: 사각형, 1: 원, 2: 삼각형
 deg = 0
+
+triangle_points = [(400, 90), (700, 500), (100, 500)]  # 삼각형 꼭짓점 미리 선언
 
 while True:
     clear_canvas_now()
     g.draw_now(400, 30)
     ch.draw_now(x, y)
 
-    if mode == 0:
+    if mode == 0:  # 사각형 운동
         if state == 0:
             x += 5
             if x > 780:
@@ -46,18 +48,31 @@ while True:
             if y < 90:
                 y = 90
                 state = 0
-                # 위치 맞춤 삭제 (아무것도 안 함)
 
-    elif mode == 1:
+    elif mode == 1:  # 원 운동
         x = 400 + 210 * math.cos(math.radians(deg) - math.pi/2)
         y = 300 + 210 * math.sin(math.radians(deg) - math.pi/2)
         deg -= 1
         if deg < -360:
             deg = 0
-            mode = 0
-            # 초기 위치도 그대로 둠
-            # 원운동 반대로
+            mode = 2
             state = 0
+
+
+    elif mode == 2:  # 삼각 운동
+        tx, ty = triangle_points[(state + 1) % 3]
+
+        dx = tx - x
+        dy = ty - y
+        dist = math.sqrt(dx * dx + dy * dy)
+
+        if dist < 5:
+            state = (state + 1) % 3
+            if state == 0:
+                mode = 0
+        else:
+            x += dx / dist * 5
+            y += dy / dist * 5
 
     delay(0.01)
     get_events()
